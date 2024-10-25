ad6b444f69a1f1948a1e993473cfbc6cbf97d652
Rara Ananta Bunga <164327639+art-trnty@users.noreply.github.com>
2024-10-26 06:19:37 +0700

    Pembaruan Files

--- LATIHAN_UTS-mvc/app_server/routes/pengguna.js
+++
@@ -1,8 +0,0 @@
-var express = require('express');
-var router = express.Router();
-
-const pgnController = require('../controllers/pengguna');
-router.get("/", pgnController.index); 
-router.post("/insert", pgnController.insert); 
-
-module.exports = router;
